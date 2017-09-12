a852855e34bb87f0370a6285bc5a8b126651e0e3
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-09-11 21:44:33 -0500

    Add index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+import { App } from './App';
+
+import './styles/app.scss';
+
+render(
+  <App />,
+  document.getElementById('app')
+);
